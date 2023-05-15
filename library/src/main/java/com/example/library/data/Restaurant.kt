0be71fd02b1f1aa7cd6233062c020bd6dbdf3d54
SRYANG <sarang628@naver.com>
2023-05-15 16:44:26 +0800

    update

--- library/src/main/java/com/example/library/data/Restaurant.kt
+++ library/src/main/java/com/example/library/data/Restaurant.kt
@@ -1,5 +1,23 @@
 package com.example.library.data
 
 data class Restaurant(
-    val name: String
+    val restaurant_id: Int? = null,
+    val restaurant_name: String? = null,
+    val address: String? = null,
+    val lat: Double? = null,
+    val lon: Double? = null,
+    val rating: Float? = null,
+    val tel: String? = null,
+    val prices: String? = null,
+    val restaurant_type: String? = null,
+    val region_code: Int? = null,
+    val review_count: Int? = null,
+    val site: String? = null,
+    val website: String? = null,
+    val img_url1: String? = null,
+    val img_url2: String? = null,
+    val img_url3: String? = null,
+    val img_url4: String? = null,
+    val img_url5: String? = null,
+    val img_url6: String? = null
 )
